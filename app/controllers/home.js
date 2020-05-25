2dbf9d35329c4183db9914fc02c83ad2a23091bd
Wesley@123 <contato@wesley.pro.br>
2020-05-25 11:17:00 -0300

    Nova versao site

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -19,9 +19,16 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/regras', (req, res, next) => {
-  // const articles = [new Article(), new Article()];
   res.render('rules', {
     title: 'ARKANYS E-SPORTS - Regras',
     members: _.union( [ first ], _.shuffle( others ) )
   });
+});
+
+router.get( ['/peticao', '/petition'], (req, res, next) => {
+  res.redirect('https://secure.avaaz.org/en/community_petitions/snail_game_improve_the_ping_brazilian_players_in_the_survival_heroes_app/details/');
+});
+
+router.get( '/discord', (req, res, next) => {
+  res.redirect('https://discord.gg/3KHm4EY');
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,74 +1,26 @@
-class HoverButton {
-  constructor(el) {
-    this.el = el;
-    this.hover = false;
-    this.calculatePosition();
-    this.attachEventsListener();
-  }
 
-  attachEventsListener() {
-    window.addEventListener('mousemove', e => this.onMouseMove(e));
-    window.addEventListener('resize', e => this.calculatePosition(e));
-    window.addEventListener('scroll', e => this.calculatePosition(e));
-  }
+	$( window ).on( 'load', () => {
 
-  calculatePosition() {
-    TweenMax.set(this.el, {
-      x: 0,
-      y: 0,
-      scale: 1
-    });
-    const box = this.el.getBoundingClientRect();
-    this.x = box.left + (box.width * 0.5);
-    this.y = box.top + (box.height * 0.5);
-    this.width = box.width;
-    this.height = box.height;
-  }
+		var instSkrollr = skrollr.init();
 
-  onMouseMove(e) {
-    let hover = false;
-    let hoverArea = (this.hover ? 0.7 : 0.5);
-    let x = e.clientX - this.x;
-    let y = e.clientY - this.y;
-    let distance = Math.sqrt( x*x + y*y );
-    if (distance < (this.width * hoverArea)) {
-       hover = true;
-        if (!this.hover) {
-          this.hover = true;
-        }
-        this.onHover(e.clientX, e.clientY);
-    }
+		if( $( window ).width() < 940 || instSkrollr.isMobile() ) {
 
-    if(!hover && this.hover) {
-      this.onLeave();
-      this.hover = false;
-    }
-  }
+			instSkrollr.destroy();
 
-  onHover(x, y) {
-    TweenMax.to(this.el, 0.4, {
-      x: (x - this.x) * 0.4,
-      y: (y - this.y) * 0.4,
-      scale: 1.15,
-      ease: Power2.easeOut
-    });
-    this.el.style.zIndex = 10;
-  }
-  onLeave() {
-    TweenMax.to(this.el, 0.7, {
-      x: 0,
-      y: 0,
-      scale: 1,
-      ease: Elastic.easeOut.config(1.2, 0.4)
-    });
-    this.el.style.zIndex = 1;
-  }
-}
+		}
 
-const btn = document.querySelectorAll('li.bullet');
+		$( window ).resize( function() {
 
-for(var i = 0; i < btn.length; i++) {
+			if( $( window ).width() < 940 || instSkrollr.isMobile() ) {
 
-  new HoverButton( btn[i].querySelector( 'div' ) );
+				instSkrollr.destroy();
 
-}
+			} else {
+
+				instSkrollr = skrollr.init();
+
+			}
+
+		} );
+
+	} );
